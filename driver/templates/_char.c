e395094f5ed5cccc77e3acd3460bb3c1d49a1e04
Carter Tsai <hamming1@gmail.com>
2013-07-28 15:09:34 +0800

    Update _char.c

--- driver/templates/_char.c
+++ driver/templates/_char.c
@@ -12,12 +12,14 @@
 #include <asm/io.h> /* inb, outb */
 
 #define SUCCESS 0
-#define FAILS   0
-
+#define FAILS   -1
+#define DRIVER_NAME "<%= _(project_name).underscored() %>"
 
 /* Global variables are declared as static, so are global whithin the file */
 
-static int major =  0;
+static int <%= _(project_name).underscored()  %>_major =  0;
+static int <%= _(project_name).underscored()  %>_minor =  0;
+static int num_of_dev = 1;
 
 /* Function declaration of <%= project_name %>.c */ 
 
@@ -50,34 +52,34 @@ struct file_operations <%= _(project_name).underscored() %>_fops = {
 static int <%= _(project_name).underscored() %>_open(struct inode *inode, struct file *filp)
 {
     printk(KERN_INFO "<%= project_name  %> : open operation\n");
-    return 0;
+    return SUCCESS;
 } 
 
 static int <%= _(project_name).underscored() %>_release(struct inode *inode, struct file *filp)
 {
     printk(KERN_INFO "<%= project_name  %> : release operation\n");
-    return 0;
+    return SUCCESS;
 }
 
 static ssize_t <%= _(project_name).underscored() %>_read(struct file *filp, char __user *buf, 
                            size_t count, loff_t *f_pos)
 {
     printk(KERN_INFO "<%= project_name  %> : read operation\n");
-    return 0;
+    return SUCCESS;
 }
 
 static ssize_t <%= _(project_name).underscored() %>_write(struct file *filp, const char __user *buf, 
                            size_t count, loff_t *f_pos)
 {
     printk(KERN_INFO "<%= project_name  %> : write operation\n");
-    return 0;
+    return SUCCESS;
 }
 
 static long <%= _(project_name).underscored() %>_ioctl(struct file *filp, unsigned int cmd, 
                             unsigned long arg)
 {
     printk(KERN_INFO "<%= project_name  %> : ioctl operation\n");
-    return 0;
+    return SUCCESS;
 }
 
 
@@ -88,8 +90,16 @@ void <%= _(project_name).underscored() %>_exit(void)
 
 int <%= _(project_name).underscored() %>_init(void)
 {
+    int result = 0;
+    dev_t dev = MKDEV(<%= _(project_name).underscored()  %>_major, <%= _(project_name).underscored()  %>_minor);
+
+    result = alloc_chrdev_region(&dev, 0, num_of_dev, DRIVER_NAME);
+
     printk(KERN_INFO "<%= project_name  %> : Hello World\n");
-    return 0;
+    return SUCCESS;
+    
+    error:
+    return FAILS;
 }
 
 module_init(<%= _(project_name).underscored() %>_init);
